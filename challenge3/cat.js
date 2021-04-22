89966a0f6d0645c638b736bc66eb6c50b94bb083
akshaysh07 <akshay.sh075@gmail.com>
2021-04-22 21:44:38 +0530

    cat generator  challenge completed

---
+++ challenge3/cat.js
@@ -0,0 +1,8 @@
+// challenge 3 cat generator
+
+function generateCat(){
+    var image= document.createElement('img');
+    var div= document.getElementById('flex-cat-gen');
+     image.src="http://thecatapi.com/api/images/get?format=src&type=gif";
+    div.appendChild(image);
+}
